b74bbde4d60e8f39ee276da9b31582d0e9aad3fc
zain <zainmd.htm@gmail.com>
2024-11-10 00:21:16 +0500

    Add Static-Resume

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleSection(section) {
+    const element = document.getElementById(section);
+    if (element.style.display === "none" || element.style.display === "") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
